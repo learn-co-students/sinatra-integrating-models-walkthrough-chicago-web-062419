c37b6fe7fbcca64431ae84c3aacc3e569a4303b0
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-07-17 17:02:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,11 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+    # text_from_user = params[:user_text]
+    # @analyzed_text = TextAnalyzer.new(text_from_user)
+    # this code can be shortened to below where :user_text can be directly used as an arguement. 
+# the instance variable @analyzed_text is an arbitary name but now it can be accessed in the erb file while also portraying the behaviors of the TextAnalyzer class
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
